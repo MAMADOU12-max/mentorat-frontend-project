2dbe61c32320b18003f6611d490212021945b590
Zachcopro <zachcopro@gmail.com>
2023-06-22 13:13:12 +0100

    debut

---
+++ src/views/HomeScreen.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+
+        <!-- Separation des section -->
+
+        <!-- section1: navbar, footer booster votre carriere  [Javis]--> 
+
+        <!-- section2: mentor et pourquoi mentor  [Mamadou]-->
+
+        <!-- Section3: Faq et temoignage [Arnold]-->
+
+
+
+        <!-- start navbar section [Javis]-->
+        <appbar></appbar>
+        <!-- end navbar section -->
+
+        <!-- start Boostez votre carriere section [Javis]-->
+        
+        <!-- end Boostez votre carriere section -->
+
+        <!-- start Nos mentors section [Mamadou-->
+        <!-- end Nos mentors section -->
+
+        <!-- start Pourquoi un mentor section [Mamadou-->
+        <!-- end Pourquoi un mentor section -->
+
+        <!-- start FAQ section [Arnold]-->
+        <!-- end FAQ section -->
+
+        <!-- start Temoignages section [Arnold]-->
+        <!-- end Temoignages section -->
+
+        <!-- start footer section [Javis]-->
+        <app-footer></app-footer>
+        <!-- end footer section -->
+        
+    </div>
+</template>
+
+
+<script>
+import Appbar from '../components/Appbar.vue'
+import AppFooter from '../components/Appfooter.vue'
+
+export default {
+    name: 'App',
+    components: {
+        Appbar, 
+        AppFooter
+    }
+}
+</script>
+
+
