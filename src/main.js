538ca147cc050de4d34acd6c5fa6ad24f5eee219
MAMADOU12-max <modoudiouf535@gmail.com>
2023-06-24 18:15:06 +0000

    working

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
+import $ from 'jquery';
+window.$ = window.jQuery = $; 
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import './index.css'
+import 'slick-carousel/slick/slick.css';
+import './index.css';
+import "./assets/slick-1.8.1/slick/slick.css";
+// import "./assets/slick-1.8.1/slick/slick.min.js";
+
 
 createApp(App).mount('#app')
--- src/views/HomeScreen.vue
+++ src/views/HomeScreen.vue
@@ -19,10 +19,31 @@
         
         <!-- end Boostez votre carriere section -->
 
-        <!-- start Nos mentors section [Mamadou-->
+        <!-- start Nos mentors section [Mamadou]-->
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h3 class="mb-4">Nos Mentors</h3>
+                     <div>
+                    <div ref="carousel" class="slick-carousel">
+                    <!-- Your carousel items here -->
+                    <div>Item 1</div>
+                    <div>Item 2</div>
+                    <div>Item 3</div>
+                    </div>
+                </div>
+
+                <div class="your-class">
+                    <div>your content</div>
+                    <div>your content</div>
+                    <div>your content</div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <!-- end Nos mentors section -->
 
-        <!-- start Pourquoi un mentor section [Mamadou-->
+        <!-- start Pourquoi un mentor section [Mamadou] -->
         <!-- end Pourquoi un mentor section -->
 
         <!-- start FAQ section [Arnold]-->
@@ -40,16 +61,49 @@
 
 
 <script>
+// import slick from "../assets/slick-1.8.1/slick/slick.min.js";
 import Appbar from '../components/Appbar.vue'
 import AppFooter from '../components/Appfooter.vue'
+// import $ from 'jquery';
+// import 'slick-carousel';
+
+// $('.your-class').slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     asNavFor: '.slider-for',
+//     dots: true,
+//     centerMode: true,
+//     focusOnSelect: true
+// });
 
 export default {
-    name: 'App',
+    name: 'App',    
     components: {
         Appbar, 
         AppFooter
-    }
+    },
+    // mounted() {
+    //     // Initialize the Slick.js carousel when the component is mounted
+    //     this.$nextTick(() => {
+    //         $(this.$refs.carousel).slick();
+    //     });
+    // },
+
+   
 }
+
+
+// export default {
+
+// };
 </script>
 
+<style>
+/* @import "@/assets/slick-1.8.1/slick/slick.css"; */
+
+/* Add any custom styles for your carousel here */
+.slick-carousel {
+    width: 100%;
+}
+</style>
 
